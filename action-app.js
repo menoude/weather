2da8abe9a2d81db223db9292a3f2c8928debd150
menoude <mennadyami@hotmail.com>
2018-11-29 17:54:48 +0000

    starting intent routing and error management

--- action-app.js
+++ action-app.js
@@ -2,18 +2,38 @@
 
 const mqtt = require('mqtt');
 
+const sessionEndMessages = {
+    'abortedByUser': 'Sorry, there was an error.',
+    'intentNotRecognized': 'Sorry, I did not understand your request.',
+    'timeout': 'Sorry, your session timed out.',
+    'error': 'Sorry, there was an error.'
+}
+
 const client  = mqtt.connect('mqtt://localhost', { port: 1883 });
 
 client.on('connect', () => {
-    client.subscribe('hermes/hotword/default/detected', (err) => console.log);
-    client.subscribe('hermes/dialogueManager/sessionEnded', (err) => console.log);
-    client.subscribe('hermes/intent/default/detected', (err) => console.log);
-    client.subscribe('hermes/intent/default/detected', (err) => console.log);
-    client.subscribe('hermes/intent/default/detected', (err) => console.log);
-    client.subscribe('hermes/intent/default/detected', (err) => console.log);
-    
+    for (let topic in hermes) {
+        client.subscribe(topic, (err) => console.log);
+    }
 });
 
-client.on('message', (topic, data) {
+client.on('message', (topic, data) => {
+    let router = new Router(topic, data);
+
     
+    if (!message)
+        console.log('unknown message');
+    else if (message == 'hotword') {
+        console.log('hotword detected');
+        console.log(data);
+    }
+    if (message == 'sessionEnd') {
+        console.log('session ended');
+        endReason = data.termination.reason;
+        answer.payload = sessionEndMessages[endReason];
+        client.publish('hermes/dialogueManager/endSession', answer.toString());
+        console.log(data);
+    } else {
+        console.log(`recognized ${message}`);
+    }    
 });
