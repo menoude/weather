6b4f890e7c778f6d8921eeba3e815b6533c06a00
menoude <mennadyami@hotmail.com>
2018-12-12 14:21:40 +0000

    fixed test generator

--- tests/generate_tests.sh
+++ tests/generate_tests.sh
@@ -5,37 +5,43 @@ valid_tests[1]="what will be the weather in 2 minutes?";
 valid_tests[2]="what will be the weather in 2 hours?";
 valid_tests[3]="what will be the weather tomorrow?";
 valid_tests[4]="what's the weather for this week end?";
-valid_tests[5]="what's the weather for next week?";
+valid_tests[5]="what's the weather next week?";
 
-invalid_tests[0]="what was the weather last week?";
 invalid_tests[1]="what was the weather 5 minutes ago?";
 invalid_tests[2]="what was the weather yesterday?";
-invalid_tests[3]="what will be the weather in 2 months?";
+invalid_tests[0]="what was the weather last week?";
+invalid_tests[5]="what will be the weather in 2 weeks?"
+invalid_tests[3]="what will be the weather in 3 months?";
 invalid_tests[4]="what will be the weather next year?";
 
 sub_topic="hermes/nlu/intentParsed";
 pub_topic="hermes/nlu/query";
 
 generate() {
+    rm $1.txt;
     if [ $1 = 'valid_tests' ]
     then
-        tests=$valid_tests;
+        tests=("${valid_tests[@]}");
+        prefix='valid_test_';
     else
-        tests=$invalid_tests;
+        tests=("${invalid_tests[@]}");
+        prefix='invalid_test_';
     fi
     mosquitto_sub -t $sub_topic > $1.txt &
     pid=$!;
-    sleep .1
-    iterator=${tests[@]};
-    echo "${iterator[@]}";
-    for query in "$iterator"
+    sleep .1;
+    for query in "${tests[@]}"
     do
-        mosquitto_pub -t $pub_topic -m "{\"input\": \"$query\"}"
-        sleep .1
+        mosquitto_pub -t $pub_topic -m "{\"input\": \"$query\"}";
+        sleep .2;
     done
-    sleep .1
-    kill $pid
-    csplit -f valid -n 1 -k ${1}.txt 1 {*}
+    sleep .2;
+    kill $pid &> /dev/null;
+    csplit -f $prefix -n 1 -k ${1}.txt 1 {*} &> /dev/null;
+    rm ${prefix}0 $1.txt;
+    echo;
+    tail -n +1 $prefix*;
+    echo;
 }
 
 generate valid_tests;
